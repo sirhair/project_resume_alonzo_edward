9edc6000d819e203a5482999b33933ba97eec470
Edward Alonzo <edward@thenewe.com>
2015-04-10 16:57:10 -0500

    use fullpage's menu

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['edwardAlonzo', 'workExperience', 'myEducation', 'mySkills', 'myInterests'],
     menu: '#pageNav'
-
   });
+  $('#pageNav').stickUp();
 });
