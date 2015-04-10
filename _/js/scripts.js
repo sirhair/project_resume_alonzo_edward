6784dfd290e7c6702fd3f584bb1fed83f52a96ef
Edward Alonzo <edward@thenewe.com>
2015-04-10 16:03:06 -0500

    add basic functionality for full page plugin

---
+++ _/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    //Navigation
+    menu: true,
+    // anchors:['firstSlide', 'secondSlide'],
+    navigation: true,
+    navigationPosition: 'right',
+    // navigationTooltips: ['firstSlide', 'secondSlide'],
+    showActiveTooltips: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+  });
+});
