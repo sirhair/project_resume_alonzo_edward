9a7ae3a7167511096547f8762679ca02eead8cc4
Edward Alonzo <edward@thenewe.com>
2015-04-10 19:43:36 -0500

    add comments to js

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+  //fullpage slides plugin
   $('#fullpage').fullpage({
     anchors: ['edwardAlonzo', 'workExperience', 'myEducation', 'mySkills', 'myInterests'],
     menu: '#pageNav'
   });
+
+  //fit text jquery plugin
   $("h1").fitText();
 
 });
