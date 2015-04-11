e3fb57087b86dc3db349516544550821de2ffe94
Edward Alonzo <edward@thenewe.com>
2015-04-10 22:23:34 -0500

    add dropdown menu functionality

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
     anchors: ['edwardAlonzo', 'workExperience', 'myEducation', 'mySkills', 'myInterests'],
     menu: '#pageNav'
   });
+  $("#pageNav li:first-of-type a").click( function(){
+    $("#pageNav li").css("display","block");
+    $("#pageNav").css("background","#fff");
+  });
+
+  $("#pageNav li + li").click( function(){
+    $("#pageNav li + li").css("display","none");
+    $("#pageNav").css("background","rgba(255,255,255, 0)");
+    console.log('hi');
+  });
 
   //fit text jquery plugin
   $("h1").fitText();
 
 });
+  
